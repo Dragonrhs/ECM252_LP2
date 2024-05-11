18805bdd8f8f3e5c49a8cd3c1dff08e34b512f41
Dragonrhs <Ryuske2009@hotmail.com>
2024-05-11 12:57:14 -0300

    <feat>[]: bootstrap

--- Aula_09_11_05/componentes-props/src/index.js
+++ Aula_09_11_05/componentes-props/src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App =() =>{
+    return(
+        <div className='container border rounded mt-2'>
+            <div className='row'>
+                <div className='col-12'>
+                    <h1 className='display 5 text-center'>Seus Pedidos </h1>
+                </div>
+            </div>
+            <div className='row'>
+                {/*<div className='col-sm-12 col-lg-6 col-xxl-3'>*/}*4
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+                <div className='col-sm-12 col-lg-6 col-xxl-3'>
+
+                </div>
+
+                <div className='col-sm-12 col-lg-6 col-xxl-3'>
+
+                </div>
+                <div className='col-sm-12 col-lg-6 col-xxl-3'>
+                
+                </div>
+                <div className='col-sm-12 col-lg-6 col-xxl-3'>
+
+                </div>                
+                
+                
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('#root')
+)
